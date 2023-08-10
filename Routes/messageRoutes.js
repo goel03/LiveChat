291e0b8de0146c5078fd2f7a788de0d7cd95c9e0
goel03 <95127071+goel03@users.noreply.github.com>
2023-08-10 09:14:15 +0530

    Update messageRoutes.js

--- Routes/messageRoutes.js
+++ Routes/messageRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const {
   allMessages,
   sendMessage,
-} = require("../controllers/messageControllers");
-const { protect } = require("../middleware/authMiddleware");
+} = require("./controllers/messageControllers");
+const { protect } = require("./middleware/authMiddleware");
 
 const router = express.Router();
 
